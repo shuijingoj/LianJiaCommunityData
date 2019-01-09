3683bf40dc7b8044af2d96049b6bf3cb79dc465e
Issac <445927866@qq.com>
2019-01-09 11:21:30 +0800

    Add exception handling.

--- code/getCommunityImg.py
+++ code/getCommunityImg.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import threading
+import time
  
 def urllib_download(save_path, img_url):
     from urllib.request import urlretrieve
@@ -24,23 +25,44 @@ def getCommunityImg(url_file, save_path):
     if not os.path.exists(save_path):
         os.makedirs(save_path)
     
-    last_community = df_img['community_id'][0]         
-    img_id = 1                                           # 图片命名为“小区ID+该小区的图片编号”
-    for i in range(0, len(df_img)):
+    start_row = len(os.listdir(save_path))
+
+    img_id = 1                     # 图片命名为“小区ID+该小区的图片编号”
+    if start_row == 0:             # 从头开始爬的情况
+        last_community = df_img['community_id'][0]
+    else:                          # 中断后继续上次进度的情况
+        last_community = df_img['community_id'][start_row-1]
+        if df_img['community_id'][start_row] == last_community:     # 上次中断时当前小区仍有图片未爬完
+            while(os.path.exists(save_path+str(df_img['community_id'][start_row])+'_%d.jpg'%img_id)):   # 找到中断时的图片编号，接着继续爬
+                img_id += 1   
+    
+    for i in range(start_row, len(df_img)):
         recent_community = df_img['community_id'][i]
         if recent_community != last_community:           # 当前小区与上一条记录的小区不一致，重新开始编号
             img_id = 1
-        urllib_download(save_path+str(df_img['community_id'][i])+'_%d.jpg'%img_id, df_img['img_url'][i])
-        img_id += 1
-        last_community = recent_community
+        try:
+            urllib_download(save_path+str(df_img['community_id'][i])+'_%d.jpg'%img_id, df_img['img_url'][i])
+            img_id += 1
+            last_community = recent_community
+            print(time.asctime(time.localtime(time.time())), url_file, '%d/%d'%(len(os.listdir(save_path)), len(df_img)))
+        except:
+            print(recent_community, ' has image downloading fail.')
 
 if __name__ == '__main__':
     df = pd.read_csv('../70cities.csv', header=None)     # 读取70个大中城市列表
     cityList = df[df[2]=='y'][1]                         # 筛选出链家上带小区信息的城市
-    city = 'sh'
 
-    for city in cityList:                                # 多线程爬取小区景观图
-        t = threading.Thread(target=getCommunityImg, args=('../community_data/%s/img.csv'%city, 'E:/小区景观图/%s/'%city))
-        t.start()
+    # 爬单城市
+    # city = 'sh'
+    # getCommunityImg('../community_data/%s/img.csv'%city, 'E:/小区景观图/%s/'%city)
 
+    # 多线程爬取全部城市小区景观图
+    # for city in cityList:                                
+    #     t = threading.Thread(target=getCommunityImg, args=('../community_data/%s/img.csv'%city, 'E:/小区景观图/%s/'%city))
+    #     t.start()
 
+    # 输出爬取图片的数量情况
+    for city in cityList:
+        location_file = '../community_data/%s/img.csv'%city
+        df_info = pd.read_csv(location_file, sep=';', error_bad_lines=False, warn_bad_lines=False)
+        print(city,'%d/%d'%(len(os.listdir('E:/小区景观图/%s/'%city)), len(df_info)))
--- code/getSateliteImg.py
+++ code/getSateliteImg.py
@@ -2,22 +2,36 @@ from getTile import *
 import pandas as pd
 import os
 import threading
+import time
 
 def getSateliteImg(location_file, save_path):
     df_info = pd.read_csv(location_file, sep=';', error_bad_lines=False, warn_bad_lines=False)
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
-    for i in range(len(df_info)):
+    start_row = len(os.listdir(save_path))
+    for i in range(start_row, len(df_info)):
         img_name = str(df_info['community_id'][i])+'.jpg'
         transformed_position = bd_To_Gcj(float(df_info['longitude'][i]), float(df_info['latitude'][i]))    
         getpic(*transformed_position, 17, source='amap', style='satellite', outfile=save_path+img_name)
+        print(time.asctime(time.localtime(time.time())), location_file, '%d/%d'%(len(os.listdir(save_path)), len(df_info)))
     
 
 if __name__ == '__main__':
     df = pd.read_csv('../70cities.csv', header=None)
     cityList = df[df[2]=='y'][1]    # 获取带有小区信息的城市缩写列表
 
+    # 爬单城市
+    # city = 'sh'
+    # getSateliteImg('../community_data/%s/baseinfo.csv'%city, 'E:/小区卫星图/%s/'%city)
+
+    # 多线程爬全部城市
     for city in cityList:
         t = threading.Thread(target=getSateliteImg, args=('../community_data/%s/baseinfo.csv'%city, 'E:/小区卫星图/%s/'%city))
         t.start()
+
+    # 输出爬取图片的数量情况
+    # for city in cityList:
+        # location_file = '../community_data/%s/baseinfo.csv'%city
+        # df_info = pd.read_csv(location_file, sep=';', error_bad_lines=False, warn_bad_lines=False)
+        # print(city,'%d/%d'%(len(os.listdir('E:/小区卫星图/%s/'%city)), len(df_info)))
